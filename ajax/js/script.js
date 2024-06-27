368aa4237735e595f2a575a072e2ba48009dd129
isabelerp <161374344+isabelerp@users.noreply.github.com>
2024-06-27 11:34:16 -0300

    Add files via upload

---
+++ ajax/js/script.js
@@ -0,0 +1,18 @@
+
+const url = 'https://api.github.com/users';
+const main = document.getElementById('main')
+// let user = 'isabelerp'
+
+function getUser(user) {
+    fetch(`${url}/${user}`)
+    .then((response) => response.json())
+    .then((data) => {
+      main.innerHTML = `${data.name} possui ${data.public_repos} repositórios
+públicos no GitHub como ${data.login}`
+    })
+    .catch((error) => console.error('Erro:', error.message || error))
+}
+const userInput = document.getElementById('username')
+userInput.addEventListener('focusout', function(event) {
+  getUser(event.target.value)
+})
